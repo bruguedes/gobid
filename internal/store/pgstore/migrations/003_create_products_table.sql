8dd08bc98ce886d702c88e22aaf68fafb859f498
bruguedes <bruguedes@gmail.com>
2025-06-05 22:41:48 -0500

    correct table

--- internal/store/pgstore/migrations/003_create_products_table.sql
+++ internal/store/pgstore/migrations/003_create_products_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS products (
     seller_id UUID NOT NULL REFERENCES users(id),
     product_name TEXT NOT NULL,
     description TEXT NOT NULL,
-    base_price FLOAT NOT NULL CHECK,
+    base_price FLOAT NOT NULL,
     auction_end TIMESTAMPTZ NOT NULL,
     is_sold BOOLEAN NOT NULL DEFAULT FALSE,
 
